c1f3cca8255dc247887fa8ce880153f9292d2532
unknown <sallyannegaller@gmail.com>
2020-09-19 11:57:42 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+// The App component should render markup matching the design.html: 
+// the main element, a header with the h1 and a div with class 'App-list'.
+// The App component will accept 1 prop, store.
+// The store prop is an object with 2 keys: lists and allCards.
+// lists is an array of objects.
+// allCards is an object where each key is a card's ID and the value is the card object with a title and content.
+// You'll be given a dummy STORE object to pass to your App.
+// The App should render a List component for each of the items in the store.lists array.
+// Each instance of the List component should be passed 2 props (and a key). The 2 props are header and cards.
+// The header prop is a string for the List's header.
+// The cards prop will be an array of card objects.
+// You'll need to combine the cardIds array for the list with the allCards object.
+
+//  
+
+import React, { Component } from 'react';
+import List from './List'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  //props defaults, to be used if no equivalent is supplied
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
+
+  render() {
+    const {store} = this.props
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            //map lists from store to List component
+            <List
+              key={list.id}
+              header={list.header}
+              //map ids in cardIds to the ids in the allCards element 
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+// The Card component should render markup matching the design.html: 
+// a div with a class of 'Card' containing an h3 for the card's title and a p element for the card's content.
+// The Card component accepts 2 props: title and content.
+// title is a string of the card's title.
+// content is a string of the card's content.
+// These props will be passed in for each Card from the List component.
+
+import React from 'react';
+import './Card.css';
+
+export default function Card(props) {
+    return (
+        <div className='Card'>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
